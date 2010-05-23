f39548a0a9e91a4baafd8c5545c3bcf941dde895
kenjiru <kenjiru.ro@gmail.com>
2010-05-23 19:29:24 +0000

    Aplicatia Java care sa testeze fisierul XSD. Plus un fisier XML de proba. O prima versiune.

---
+++ lab9/src/app/Main.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+public class Main {
+	static final String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+	static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+	static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
+
+	public static void main(String[] args) {
+		String xmlFile = "test.xml";
+		String xsdFile = "magazin.xsd";
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		factory.setNamespaceAware(true);
+		factory.setValidating(true);
+		
+		try {
+			factory.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+			factory.setAttribute(JAXP_SCHEMA_SOURCE, new File(xsdFile)); 
+		} 
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.parse(new File(xmlFile));
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Documentul a fost validat!");
+	}
+}
